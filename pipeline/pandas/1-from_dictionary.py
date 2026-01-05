fe3e17cc59d0cdad78ee8723693ba92a7f9b2c9b
Semedw <smusazade2000@gmail.com>
2026-01-05 16:33:00 +0400

    pep8

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,4 +5,6 @@ from dict
 
 import pandas as pd
 
-df = pd.DataFrame({"First" : [0.0, 0.5, 1.0, 1.5], "Second" : ['one', 'two','three', 'four']}, index=["A", "B", "C", "D"])
+df = pd.DataFrame({"First": [0.0, 0.5, 1.0, 1.5], 
+    "Second": ['one', 'two', 'three', 'four']}, 
+    index=["A", "B", "C", "D"])

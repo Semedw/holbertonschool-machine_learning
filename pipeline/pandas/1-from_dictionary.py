2ef266a470739c70124576cac714a79327c6f4f8
Semedw <smusazade2000@gmail.com>
2026-01-05 16:28:00 +0400

    added index

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,4 +5,4 @@ from dict
 
 import pandas as pd
 
-df = pd.DataFrame({"First" : [0.0, 0.5, 1.0, 1.5], "Second" : ['one', 'two','three', 'four']}
+df = pd.DataFrame({"First" : [0.0, 0.5, 1.0, 1.5], "Second" : ['one', 'two','three', 'four']. index=['A', 'B', 'C', 'D']}

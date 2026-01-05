9d7386fbdab02770f4d8349cd18c116d8083e54d
Semedw <smusazade2000@gmail.com>
2026-01-05 17:52:33 +0400

    salam

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -3,13 +3,12 @@
 slicing data frame
 """
 
-if __init__ == "__main__":
 
-    def slice(df):
-        """
-        inside the func
-         """
-        cols = ['High', 'Low', 'Close', 'Volume_BTC']
-        df = df[cols]
-        df_sliced = df.iloc[::60]
-        return df_sliced
+def slice(df):
+    """
+    inside the func
+    """
+    cols = ['High', 'Low', 'Close', 'Volume_BTC']
+    df = df[cols]
+    df_sliced = df.iloc[::60]
+    return df_sliced

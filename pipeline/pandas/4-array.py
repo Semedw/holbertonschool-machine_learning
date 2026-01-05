37074c1a9916b248dcb584581f631012e192228c
Semedw <smusazade2000@gmail.com>
2026-01-05 17:25:29 +0400

    to numpy

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+to numpy
+"""
+
+import pandas as pd
+
+
+def array(df):
+    """
+    inside the func
+    """
+    df = df.tail(10)[['High', 'Close']]
+    numpay_array = df.to_numpy()
+    return numpy_array

036903bdee6b88e4670868a2edbdbfb588c04975
Semedw <smusazade2000@gmail.com>
2026-01-08 16:45:05 +0400

    prune

---
+++ pipeline/pandas/8-prune.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+filtering the dataset
+"""
+
+
+def prune(df):
+    """
+    removing(filtering) close entries with NaN values
+    """
+    df = df[df["Close"] != 'NaN']]
+    return df

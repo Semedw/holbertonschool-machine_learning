643a9dc2bd6f9054e60e8c13bcb8bc0b011c7301
Semedw <smusazade2000@gmail.com>
2026-01-08 16:39:19 +0400

    sort by high

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+sort high
+"""
+
+
+def high(df):
+    """
+    sorting the dataframe by high values
+    """
+    sorted_df = df.sort_value(by="High", ascending = False)
+    return sorted_df

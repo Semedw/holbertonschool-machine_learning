c0ff499263510c6786bb2323b5185330a606b3a3
Semedw <smusazade2000@gmail.com>
2026-01-08 17:39:30 +0400

    ffill

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -9,7 +9,7 @@ def fill(df):
     removing weighted_price column and filling nan values
     """
     df = df.drop('Weighted_Price', axis=1, errors='ignore')
-    df["Close"] = df["Close"].fillna(df["Close"].shift(1))
+    df["Close"] = df["Close"].fillna(method='ffill')
     for col in ['High', 'Low', 'Open']:
         df[col] = df['Close']
     df[["Volume_(BTC)", "Volume_(Currency)"]] = (

05e67043586aad5a8201c2d50381ce9e9e6dba32
Semedw <smusazade2000@gmail.com>
2026-01-09 17:15:12 +0400

    concatenating

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+doing multiple things but concatenating is more significant
+"""
+
+index = __import__('10-index').index
+
+def concat(df1, df2):
+    """
+    concatenating dataframes
+    """
+    df1 = df1.set_index('Timestamp')
+    df2 = df2.set_index('Timestamp')
+    concatenated_df = pd.concat([df1, df2], keys=['coinbase', 'bitstamp'])
+    return concatenated_df

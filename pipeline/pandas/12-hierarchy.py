6331d1976f33c50caa7ebd0906ff44090a7916cc
Semedw <smusazade2000@gmail.com>
2026-01-09 18:17:35 +0400

    reorder level

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -17,5 +17,6 @@ def hierarchy(df1, df2):
     df1 = df1.loc[1417411980:1417417980]
     df2 = df2.loc[1417411980:1417417980]
     concatenated_df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
-    df_sorted = concatenated_df.sort_values(by='Timestamp', ignore_index=True)
+    concatenated_df = concatenated_df.reorder_levels(['Timestamp', 'exchange']
+    df_sorted = concatenated_df.sort_index()
     return concatenated_df

759115c78b0c8efe7c5e884dc2e55db326562c5b
Semedw <smusazade2000@gmail.com>
2026-01-09 18:56:35 +0400

    trying smth

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -16,8 +16,6 @@ def hierarchy(df1, df2):
     df2 = index(df2)
     df1 = df1.loc[1417411980:1417417980]
     df2 = df2.loc[1417411980:1417417980]
-    concatenated_df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'],
-                                names=['exchange'])
-    concatenated_df = concatenated_df.reorder_levels(order=['Timestamp', 'exchange'])
+    concatenated_df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
     concatenated_df = concatenated_df.sort_index()
     return concatenated_df

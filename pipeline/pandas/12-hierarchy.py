df472a500e918ab9941b08fed456ff6ca77f36ea
Semedw <smusazade2000@gmail.com>
2026-01-09 18:00:24 +0400

    from to

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+building hierarchy
+"""
+
+import pandas as pd
+
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """
+    rearranging multiindex
+    """
+    df1 = index(df1)
+    df2 = index(df2)
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
+    concatenated_df = pd.concat(['df2', 'df1'], keys=['bitstamp', 'coinbase'])
+    
+    return concatenated_df

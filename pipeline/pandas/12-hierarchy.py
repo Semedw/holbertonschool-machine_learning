04856383503bfffb5a5853dcbd630a96980891d4
Semedw <smusazade2000@gmail.com>
2026-01-09 18:24:32 +0400

    added a name to keys

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -16,7 +16,8 @@ def hierarchy(df1, df2):
     df2 = index(df2)
     df1 = df1.loc[1417411980:1417417980]
     df2 = df2.loc[1417411980:1417417980]
-    concatenated_df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    concatenated_df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'],
+                                names = ['exchange'])
     concatenated_df = concatenated_df.reorder_levels(['Timestamp', 'exchange']
     df_sorted = concatenated_df.sort_index()
     return df_sorted

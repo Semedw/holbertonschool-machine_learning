ce4a8d63fb477126d7db1f24ecf0bd9deb1b47fa
Semedw <smusazade2000@gmail.com>
2026-01-10 21:29:01 +0400

    corrected the image

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -10,5 +10,9 @@ def line():
     # your code here
     x = np.arange(0, 11)
     plt.plot(x, y, color='#FF0000')
+    plt.xlim(left=0, right=0)
     plt.xticks(np.arange(0, 11, 2))
-    plt.show()
+    plt.savefig("my_plot.png")   # write it to disk
+    plt.close()
+
+line()

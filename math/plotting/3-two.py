0eef23f99b3829e968abb4e8baa8f12534bb94c7
Semedw <smusazade2000@gmail.com>
2026-01-12 18:20:40 +0400

    corrected the syntax error

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -20,12 +20,12 @@ def two():
     plt.figure(figsize=(6.4, 4.8))
 
     # your code here
+    plt.plot(x, y1, linestyle='dashed', label='C-14')
+    plt.plot(x, y2, color='green', label='Ra-226')
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of Radioactive Elements')
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
-    plt.plot(x, y1, linestyle='dashed', label='C-14')
-    plt.plot(x, y2, color='green', label='Ra-226')
     plt.legend(loc='upper right')
-    plt.xticks(np.arange(0, 20000, 2500)
+    plt.xticks(np.arange(0, 20000, 2500))

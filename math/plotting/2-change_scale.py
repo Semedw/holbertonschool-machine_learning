903de4908b632544dcf5f3fd5597b9d6999ed085
Semedw <smusazade2000@gmail.com>
2026-01-12 16:58:54 +0400

    scaling

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+i added the docs this time
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """
+    changing range
+    """
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(x, y)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Exponential Decay of C-14')
+    plt.yscale('log')
+    plt.xlim(0, 28650)

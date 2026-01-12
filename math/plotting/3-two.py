eb5055c2c76be2da2517e6ea485c7aa013ffb1dd
Semedw <smusazade2000@gmail.com>
2026-01-12 18:16:42 +0400

    changed x ticks

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -25,6 +25,7 @@ def two():
     plt.title('Exponential Decay of Radioactive Elements')
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
+    plt.xticks(np.arange(0, 20000, 2500)
     plt.plot(x, y1, linestyle='dashed', label='C-14')
     plt.plot(x, y2, color='green', label='Ra-226')
     plt.legend(loc='upper right')

e159327d806cbc2efda24c7497b1892d41ec679d
Semedw <smusazade2000@gmail.com>
2026-01-15 15:44:02 +0400

    salam

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-creating multiple colored bars
+creating a stacked bar chart of fruit per person
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,36 +8,33 @@ import matplotlib.pyplot as plt
 
 def bars():
     """
-    inside the function
+    Plots a stacked bar chart for fruit quantities
     """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
-    plt.figure(figsize=(6.4, 4.8))
+    fruit = np.random.randint(0, 20, (4, 3))  # 4 types of fruit, 3 people
 
-    # your code here
-    
-    people = ['Farrah', 'Fred', 'Felicia']
-    x = np.arange(len(people))
-    fruit_names = ['apples', 'bananas', 'oranges', 'peaches']
-    fruit_colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-    w = 0.5
-    bottom = np.zeros(len(people))
-    
-    for i in range(len(fruit)):
-        plt.bar(x, fruit[i], width=w, bottom=bottom, color=fruit_colors[i],
-                label=fruit_names[i])
-        bottom += fruit[i]
+    cats = ['Farrah', 'Fred', 'Felicia']
+    x = np.arange(len(cats))
+    width = 0.5
 
-    plt.xticks(x, people)
+    # Colors and labels for each row of fruit
+    labels = ['apples', 'bananas', 'oranges', 'peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+
+    # Stack bars
+    bottom = np.zeros(len(cats))
+    for i in range(fruit.shape[0]):
+        plt.bar(x, fruit[i], width, bottom=bottom, label=labels[i], color=colors[i])
+        bottom += fruit[i]  # increment bottom for next fruit
+
+    # Axis labels, limits, ticks
+    plt.xticks(x, cats)
     plt.ylabel('Quantity of Fruit')
     plt.ylim(0, 80)
     plt.yticks(np.arange(0, 81, 10))
-    plt.legend()
 
+    # Legend and title
+    plt.legend()
     plt.title('Number of Fruit per Person')
 
-    # plt.savefig('my_plot')
-    # plt.close()
     plt.show()
-
-bars()

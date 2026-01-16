6d2b95b245c41fd0207ff9ee69d82c80fc4cb9d2
Semedw <smusazade2000@gmail.com>
2026-01-16 12:13:09 +0400

    toreme

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+derivative of polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+    calculating the derivative
+    """
+    if len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    der = []
+    for i in range(1, len(poly)):
+        der.append(poly[i] * i)
+    return der
+
+print(poly_derivative([5, 3, 0, 1]))
+

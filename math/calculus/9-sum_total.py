78cdfa193a97afdf97e1757c2766b356e93f130f
Semedw <smusazade2000@gmail.com>
2026-01-16 11:53:27 +0400

    ededi silsile cem dusturu

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+total sum
+"""
+
+
+def summation_i_squared(n):
+    """
+    sum of squared numbers
+    """
+    if isinstance(n, int):
+        s = n * (n+1) / 2
+        return s
+    return None

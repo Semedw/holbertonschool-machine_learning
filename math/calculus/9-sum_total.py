a8e1df92b3ed343d93be4de8f0a17d84bd4c6965
Semedw <smusazade2000@gmail.com>
2026-01-16 12:01:27 +0400

    salam

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,6 +9,9 @@ def summation_i_squared(n):
     sum of squared numbers
     """
     if isinstance(n, int):
-        s = n * (n + 1) * (2 * n + 1) / 6
-        return s
+        if i > 1:
+            s = n * (n + 1) * (2 * n + 1) / 6
+            return s
+        else:
+            return None
     return None

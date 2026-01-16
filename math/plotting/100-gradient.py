b91f55eb1400cd504ad48a91d36f45ca672fbd2b
Semedw <smusazade2000@gmail.com>
2026-01-16 13:01:03 +0400

    salam

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+gradient
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """
+    making colorbar
+    """
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    
+    plt.scatter(x, y, c=z, cmap='viridis')
+    
+    data_2d = z.reshape(40, 50)
+    im = plt.imshow(data_2d, cmap='viridis')
+    plt.colorbar(im)
+    plt.xlabel('x coordinates (m)')
+    plt.xticks(np.arange(-30, 31, 10))
+    plt.xlim(-40, 40)
+    plt.ylabel('y coordinates (m)')
+    plt.yticks(np.arange(30, -31, -10))
+    plt.ylim(-40, 40)
+    plt.title('Mountain Elevation')
+    plt.show()
+
+gradient()

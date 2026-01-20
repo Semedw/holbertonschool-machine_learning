ed90285952463f74bc68bb867a1a707a7128b616
Semedw <smusazade2000@gmail.com>
2026-01-20 16:11:04 +0400

    salam

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+elementwise numpy
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    applying elementwise operations
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)

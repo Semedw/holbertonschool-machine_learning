aff2325b848252afe59e74d2453ba9d9da50d2d2
Semedw <smusazade2000@gmail.com>
2026-01-20 16:18:58 +0400

    salam

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+saddle up
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    matrix multiplication
+    """
+    result = np.matmul(mat1, mat2)
+    return result

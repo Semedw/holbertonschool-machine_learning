4c8147c04d5b9c85ddb2844a6420ecb537f9bbc5
Semedw <smusazade2000@gmail.com>
2026-01-20 16:02:15 +0400

    salam

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -8,12 +8,5 @@ def np_shape(matrix):
     """
     calculating the shape of matrix
     """
-    shape = []
-    m = matrix
-    while isinstance(m, list):
-        shape.append(len(m))
-        if len(m) != 0:
-            m = m[0]
-        else:
-            break
-    return tuple(shape)
+    shape = matrix.shape()
+    return shape

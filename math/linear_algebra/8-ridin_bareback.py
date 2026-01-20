4c1f7eb52eccc71c8c12ef266fd55b570b4b2898
Semedw <smusazade2000@gmail.com>
2026-01-20 15:40:19 +0400

    salam

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -10,7 +10,7 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    result = [[0 for i in range(len(mat2[0]))] * len(mat1)]
+    result = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]
 
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):

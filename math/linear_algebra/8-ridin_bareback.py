6665804ca924d86292d9d89c69fe263650cfceea
Semedw <smusazade2000@gmail.com>
2026-01-20 15:36:29 +0400

    3 nested loops

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -10,17 +10,10 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    result = []
+    result = [[0 for _ in range(len(mat2[0]))], for _ in range(len(mat1))]
+
     for i in range(len(mat1)):
-        for j in range(len(mat1[i])):
-            s= []
-            for k in range(len(mat2[j])):
-                s.append(mat1[i][j] * mat2[j][k])
-            result.append(s)
-    last = []
-    for i in range(0, len(result)-1, len(mat2)):
-        s = []
-        for j in range(len(result[i])):
-            s.append(result[i][j] + result[i+1][j])
-        last.append(s)
-    return last
+        for j in range(len(mat2[0])):
+            for k in range(len(mat1[0])):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result

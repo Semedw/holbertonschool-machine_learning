d6ae4e5d7cb1734e7504b8d5bb627a661159d011
Semedw <smusazade2000@gmail.com>
2026-01-20 14:03:43 +0400

    salam

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,10 +6,6 @@ getting cozy
 
 def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
-        if len(mat1) != len(mat2):
-            return None
-        if len(mat1[0]) != len(mat2[0]):
-            return None
         result = mat1 + mat2
     else:
         result = []
@@ -17,4 +13,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
             return None
         for i in range(len(mat1)):
             result.append(mat1[i] + mat2[i])
+    lens = []
+    for i in result:
+        lens.append(len(i))
+    if lens.count(lens[0]) != len(lens):
+        return None
     return result

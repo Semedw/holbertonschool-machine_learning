74ba3676e489ce730fbae27897f65902daec4502
Semedw <smusazade2000@gmail.com>
2026-01-21 15:42:17 +0400

    salam

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -32,5 +32,5 @@ def cat_matrices(mat1, mat2, axis=0):
     """
     if not same_shape(mat1, mat2):
         return None
-    concat = np.concatenate((mat1, mat2), axis=axis)
+    concat = mat1 + mat2
     return concat

d35e794159074dd70714805cd2adc48b5f049081
Semedw <smusazade2000@gmail.com>
2026-01-21 15:44:43 +0400

    salam

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -4,31 +4,30 @@ squashed like sardines
 """
 
 
-def same_shape(mat1, mat2):
-    """'
-    checks if the matrices are matching
-    """
-    if isinstance(mat1, list) != isinstance(mat2, list):
-        return False
-
-    if not isinstance(mat1, list):
-       return True
-
-    if len(mat1) != len(mat2):
-        return False
-    
-    for x, y in zip(mat1, mat2):
-        if not same_shape(x, y):
-            return False
-
-    return True
-
-    
 def cat_matrices(mat1, mat2, axis=0):
-    """
-    concatenating matrices
-    """
-    if not same_shape(mat1, mat2):
+    # Helper: get shape of a nested list
+    def get_shape(mat):
+        if isinstance(mat, list):
+            return [len(mat)] + get_shape(mat[0])
+        return []
+
+    shape1 = get_shape(mat1)
+    shape2 = get_shape(mat2)
+
+    # Check if axis is valid
+    if axis < 0 or axis >= len(shape1):
         return None
-    concat = mat1 + mat2
-    return concat
+
+    # Check compatibility on all axes except the concatenation axis
+    for i, (s1, s2) in enumerate(zip(shape1, shape2)):
+        if i != axis and s1 != s2:
+            return None
+
+    # Recursive concatenation
+    def concat_recursive(m1, m2, ax):
+        if ax == 0:
+            return m1 + m2
+        else:
+            return [concat_recursive(m1[i], m2[i], ax-1) for i in range(len(m1))]
+
+    return concat_recursive(mat1, mat2, axis)

22d0cc729deaa7842613980691091ea6c6b7ecd1
Semedw <smusazade2000@gmail.com>
2026-01-21 15:08:50 +0400

    salam

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -9,6 +9,7 @@ def add_matrices(mat1, mat2):
     adding matrices
     """
     if not isinstance(mat1, list):
-        return [a+b for a, b in zip(mat1, mat2)]
+        return mat1 + mat2
+    
     return [add_matrices(matrix1, matrix2) for matrix1, matrix2 in zip(mat1,
                                                                       mat2)]

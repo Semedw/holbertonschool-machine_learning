7a8a538faba93f38ae4f005c993186df4b3e0ec8
Semedw <smusazade2000@gmail.com>
2026-01-21 14:04:25 +0400

    salam

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -11,10 +11,15 @@ def np_slice(matrix, axes={}):
     """
     newMat = matrix
     result = []
+    idx = [slice(None)] * matrix.ndim
     for key, value in axes.items(): 
-        # sec = slice(*value)
-        newMat = newMat.take(indices=range(*value), axis=key)
-    return newMat
+        sec = slice(*value)
+        idx[key] = slice(*value)
+    return newMat[tuple(idx)]
 
 # mat1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 # print(np_slice(mat1, axes={1: (1, 3)}))
+# mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+#                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+#                [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+# print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))

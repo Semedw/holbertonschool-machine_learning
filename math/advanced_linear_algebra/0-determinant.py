0c70bb6f71cd503fe63deb2802cf959fd6dd9485
Semedw <smusazade2000@gmail.com>
2026-01-27 18:22:47 +0400

    checked if the matrix itself is a list

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -8,17 +8,16 @@ def determinant(matrix):
     """
     finding the determinant of matrix
     """
-    if isinstance(matrix[0], list):
-        if len(matrix) != len(matrix[0]):
-            raise ValueError('matrix must be a square')
+    if isinstance(matrix, list):
+        if isinstance(matrix[0], list):
+            if len(matrix) != len(matrix[0]):
+                raise ValueError('matrix must be a square')
+            else:
+                if len(matrix) == 1:
+                    return matrix[0][0]
+                if len(matrix) == 2:
+                    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
         else:
-            if len(matrix) == 1:
-                return matrix[0][0]
-            if len(matrix) == 2:
-                return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-
-
-
+            raise TypeError('matrix must be a list of lists')
     else:
         raise TypeError('matrix must be a list of lists')

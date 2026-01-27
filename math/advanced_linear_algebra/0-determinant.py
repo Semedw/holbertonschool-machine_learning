e5137a30096acbf0a8928c231965c80ada1eab05
Semedw <smusazade2000@gmail.com>
2026-01-27 18:31:40 +0400

    salam

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -21,6 +21,7 @@ def determinant(matrix):
         if len(matrix) == 1:
             return matrix[0][0]
         if len(matrix) == 2:
-            return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+            det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+            return det
     else:
         raise TypeError('matrix must be a list of lists')

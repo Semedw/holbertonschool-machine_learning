454a0309da9023797bda755bd3b0ac1f38ffa9d3
Semedw <smusazade2000@gmail.com>
2026-01-28 12:36:36 +0400

    minor matrix of matrix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -4,6 +4,34 @@ function for finding minor
 """
 
 
+def determinant(matrix):
+    """ 
+    finding the determinant of matrix
+    """
+    if isinstance(matrix, list):
+        for i in matrix:
+            if not isinstance(i, list):
+                raise TypeError('matrix must be a list of lists')
+        k = len(matrix)
+        for i in matrix:
+            if len(i) == 0:
+                return 1
+            if len(i) != k:
+                raise ValueError('matrix must be a square matrix')
+        if len(matrix) == 1:
+            return matrix[0][0]
+        if len(matrix) == 2:
+            det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+            return det 
+        det = 0 
+        for col in range(len(matrix)):
+            sub = [row[:col] + row[col+1:] for row in matrix[1:]]
+            det += ((-1)**col * matrix[0][col] * determinant(sub))
+        return det 
+    else:
+        raise TypeError('matrix must be a list of lists')
+
+
 def minor(matrix):
     """
     finding minor
@@ -24,6 +52,9 @@ def minor(matrix):
                        i != r]
                 if len(sub) == 1:
                     new.append(sub[0][0])
-
+                else:
+                    new.append(determinant(sub))
+            minor.append(new)
+        return minor
     else:
         raise TypeError('matrix must be a list of lists')

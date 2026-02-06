9cf8ea82cfe4aa8bd2a34af44082f33adab349fe
Semedw <smusazade2000@gmail.com>
2026-02-06 21:29:07 +0400

    salam

---
+++ math/probability/normal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+initializing normal dist class
+"""
+
+
+class Normal:
+    """
+    inside the normal class
+    """
+
+    def __init__(self, data=None, mean=0, stddev=1.):
+        """
+        initializing the object
+        """
+        self.data = data
+        self.mean = mean
+        self.stddev = stddev
+
+        if data is None:
+            self.mean = mean
+            self.stddev = stddev
+            if stddev < 0:
+                raise ValueError('stddev must be a positive value')
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.mean = sum(data) / len(data)
+            

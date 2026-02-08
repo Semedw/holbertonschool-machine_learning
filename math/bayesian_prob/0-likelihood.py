dbf697f6403fef6bc573b796ca958070d1eaad18
Semedw <smusazade2000@gmail.com>
2026-02-08 22:43:12 +0400

    salam

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+finding likelihood
+"""
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    inside the function
+    """
+    res = np.array()
+    if n<=0:
+        raise ValueError('n must be a positive integer')
+    if not isinstance(x, int) or x < 0:
+        raise ValueError('x must be an integer that is greater than or equal to
+        0')
+    if x > n:
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.shape != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+    for i in P:
+        if i < 0 or i > 1:
+            raise ValueError('All values in P must be in the range [0, 1]')
+    for i in P:
+        lk = i * n / x
+        res.append(lk)
+    return res

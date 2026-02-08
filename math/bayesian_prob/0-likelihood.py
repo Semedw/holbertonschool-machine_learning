e03a651f22f73c88f711ec1b58d784716182698e
Semedw <smusazade2000@gmail.com>
2026-02-08 22:53:41 +0400

    salam

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -20,7 +20,7 @@ def likelihood(x, n, P):
     for i in P:
         if i < 0 or i > 1:
             raise ValueError('All values in P must be in the range [0, 1]')
-    if not isinstance(P, np.ndarray) or P.shape != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
     for i in P:
         lk = i * n / x

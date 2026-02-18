25b6059f08eb36a0596b3a704470a079f86fef4a
Semedw <smusazade2000@gmail.com>
2026-02-18 18:17:45 +0400

    salam

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -10,8 +10,9 @@ def sensitivity(confusion):
     '''
     inside the func
     '''
-
+    res = []
     for c in range(len(confusion)):
         tp = confusion[c, c]
         fn = sum(confusion[c,:]) - confusion[c,c]
-    return tp/(tp+fn)
+        res.append(tp/(tp+fn))
+    return np.ndarray(res)

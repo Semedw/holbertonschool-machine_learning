86a420025c32985ce44042d05ed1cf5a56968e18
Semedw <smusazade2000@gmail.com>
2026-02-18 18:45:14 +0400

    salam

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -4,16 +4,15 @@ f1 score
 '''
 
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 def f1_score(confusion):
     '''
     inside the func
     '''
-    res = []
-    for c in range(len(confusion)):
-        tp = confusion[c, c]
-        fp = sum(confusion[:,c]) - confusion[c, c]
-        fn = sum(confusion[c,:]) - confusion[c, c]
-        tn = sum(np.delete(sum(confusion)-confusion[c, :],c))
-        res.append(2*tp/(2*tp + fp + fn))
-    return np.array(res)
+    pr = precision(confusion)
+    rec = sensitivity(confusion)
+
+    f_value = 2/(1/pr + 1/rec)
+    return f_value

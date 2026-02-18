cba2ab5c667e982ca8afeb01b37cf1a413e35d34
Semedw <smusazade2000@gmail.com>
2026-02-18 18:27:42 +0400

    salam

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+calculating precision
+'''
+
+import numpy as np
+
+
+def precision(confusion):
+    '''
+    inside the func
+    '''
+    res = []
+    for c in range(len(confusion)):
+        tp = confusion[c, c]
+        fp = sum(confusion[:,c]) - confusion[c,c]
+        res.append(tp / (tp + fp))
+    return np.array(res)

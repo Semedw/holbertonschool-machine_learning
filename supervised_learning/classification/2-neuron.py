84691ce160069035d40a092cccbbf2ddf08655b7
Semedw <smusazade2000@gmail.com>
2026-03-02 18:18:35 +0400

    salam

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -17,7 +17,7 @@ class Neuron:
             raise TypeError('nx must be an integer')
         elif nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.__W = np.random.randn(nx ,1)
+        self.__W = np.random.randn(nx, 1)
         self.__b = 0
         self.__A = 0
 
@@ -46,6 +46,6 @@ class Neuron:
         '''
         calculating forward propagation
         '''
-        Z = np.matmul(X, self.__W) + self.__b
+        Z = np.matmul(X, self.__W.T) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A

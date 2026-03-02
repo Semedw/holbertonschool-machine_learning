1597794cddb1670c36c050815c9ba5e82fe1a912
Semedw <smusazade2000@gmail.com>
2026-03-02 18:21:49 +0400

    salam

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -46,6 +46,6 @@ class Neuron:
         '''
         calculating forward propagation
         '''
-        Z = np.dot(self.__W, X) + self.__b
+        Z = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A

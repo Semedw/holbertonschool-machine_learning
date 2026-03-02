cc5a8c363f6669d214cdbaf59593269d417bddd7
Semedw <smusazade2000@gmail.com>
2026-03-02 17:13:13 +0400

    salamg

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+'''
+writing neural network
+'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''
+    neuron class
+    '''
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+
+    @property
+    def W(self):
+        '''
+        Weight getter
+        '''
+        return self.__W
+    
+    @property
+    def b(self):
+        '''
+        Bias getter
+        '''
+        return self.__b
+    
+    @property
+    def A(self):
+        '''
+        Activated neuron getter
+        '''
+        return self.__A

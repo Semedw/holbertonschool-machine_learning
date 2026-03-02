03e7309a7a2900b6a29296f6604290eef82e1f6e
Semedw <smusazade2000@gmail.com>
2026-03-02 17:05:57 +0400

    salam

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -17,6 +17,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         elif nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.randn(shape=1, nx)
+        self.W = np.random.randn(nx, shape=1)
         self.b = 0
         self.A = 0

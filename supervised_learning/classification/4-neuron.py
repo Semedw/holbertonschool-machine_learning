a33070c5a71d131cf1f14bdc327105c885c343a1
Semedw <smusazade2000@gmail.com>
2026-03-02 19:18:10 +0400

    datas

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+'''
+writing neural network
+'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''
+    neuron class
+    '''
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''
+        Weight getter
+        '''
+        return self.__W
+
+    @property
+    def b(self):
+        '''
+        Bias getter
+        '''
+        return self.__b
+
+    @property
+    def A(self):
+        '''
+        Activated neuron getter
+        '''
+        return self.__A
+
+    def forward_prop(self, X):
+        '''
+        calculating forward propagation
+        '''
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        '''
+        loss (cost) function
+        '''
+        m = Y.shape[1]
+        sq = (Y - A) ** 2
+        loss = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001-A))
+        cost = (1 / m) * np.sum(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        '''
+        Evaluates the neuron's predictions
+        '''
+        

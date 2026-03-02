4e8e9a57acfbc1a20817909d10e710053e6af223
Semedw <smusazade2000@gmail.com>
2026-03-02 16:38:48 +0400

    salam

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+writing neural network
+'''
+
+
+import numpy as np
+
+
+class Neuron:
+    '''
+    neuron class
+    '''
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+
+    W = np.random.normal
+    b = 0
+    A = 0

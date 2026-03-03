11f9ac667e3e7ba98390ad1078e2f9cbe552201c
Semedw <smusazade2000@gmail.com>
2026-03-03 17:52:48 +0400

    salam

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -75,7 +75,7 @@ class Neuron:
 
         error = A - Y
 
-        m = len(Y)
+        m = Y.shape[1]
         gradient_weight  = (1 / m) * np.matmul(error, X.T)
         gradient_bias = (1 / m) * np.sum(error)
         self.__W = self.__W - alpha * gradient_weight

e18ea15fba45829a7a6e5afaa3417ab065fcf57b
Semedw <smusazade2000@gmail.com>
2026-03-03 17:20:48 +0400

    salam

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -72,7 +72,7 @@ class Neuron:
         '''
         this function calculates one pass of gradient descent
         '''
-        h = np.dot(self.__W.T, X)
+        h = np.dot(X, A)
 
         error = h - Y
 

d5fce4118de6a62c3d756de0fc1d3fbf8cc04985
Semedw <smusazade2000@gmail.com>
2026-03-04 15:36:04 +0400

    salam

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -24,6 +24,6 @@ class NeuralNetwork:
         self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nx, 1))
         self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
+        self.W2 = np.random.randn(1, nx)
         self.b2 = 0
         self.A2 = 0

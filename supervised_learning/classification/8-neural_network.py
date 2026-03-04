d0d860d456877b3cb418fa4db0c2e055b1026d57
Semedw <smusazade2000@gmail.com>
2026-03-04 15:29:51 +0400

    salam

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -21,7 +21,7 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError('nodes must be a postive integer')
         
-        self.W1 = np.random.randn(1, nx)
+        self.W1 = np.random.randn(1, nodes)
         self.b1 = np.zeros(nx)
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)

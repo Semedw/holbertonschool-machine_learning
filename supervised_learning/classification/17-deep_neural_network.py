fe39fb9ead800dece2aacb58f41bd5e941b0ec47
Semedw <smusazade2000@gmail.com>
2026-03-05 11:03:29 +0400

    salam

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -3,45 +3,45 @@
 deep neural network
 '''
 
+
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Defines a deep neural network for binary classification"""
+    '''
+    building a deep neural network
+    '''
 
     def __init__(self, nx, layers):
-        """
-        Initializes the Deep Neural Network
-        nx: number of input features
-        layers: list representing the number of nodes in each layer
-        """
-        # Validate nx
+        '''
+        nx - the number of input features -> int
+        layers - the number of nodes in eahc layer of the network -> List
+        e.g. layers[0] represents the the number of nodes in the first layer
+        '''
+
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer")
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
+            raise ValueError('nx must be a positive integer')
 
-        # Validate layers
         if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
+            raise TypeError('layers must be a list of positive integers')
 
-        self.__L = len(layers)
-        self.__cache = {}
-        self.__weights = {}
-
-        for l in range(self.L):
-            # Validate layer elements
-            if not isinstance(layers[l], int) or layers[l] <= 0:
+        self.L = len(layers)  # the number of layers in the neural network
+        self.cache = {}  # a dict to hold all intermediary values of the network
+        self.weights = {}
+        for lay in range(self.L):
+            if layers[lay] < 1 or type(layers[lay]) is not int:
                 raise TypeError("layers must be a list of positive integers")
-
-            # Determine input size for the current layer
-            # If it's the first layer, input size is nx, else it's nodes in previous layer
-            prev_layer_size = nx if l == 0 else layers[l-1]
-
-            # He et al. initialization
-            self.weights[f"W{l + 1}"] = np.random.randn(layers[l], prev_layer_size) * np.sqrt(2 / prev_layer_size)
-            # Bias initialized to zeros
-            self.weights[f"b{l + 1}"] = np.zeros((layers[l], 1))
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
+            if lay == 0:
+                He_val = np.random.randn(layers[lay], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str(lay + 1)] = He_val
+            if lay > 0:
+                He_val1 = np.random.randn(layers[lay], layers[lay - 1])
+                He_val2 = np.sqrt(2 / layers[lay - 1])
+                He_val3 = He_val1 * He_val2
+                self.weights["W" + str(lay + 1)] = He_val3
 
     @property
     def L(self):

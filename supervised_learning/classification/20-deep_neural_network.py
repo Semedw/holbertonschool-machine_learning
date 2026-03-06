f1df1760f449ecceefac54cdecd7f7204d87cc0c
Semedw <smusazade2000@gmail.com>
2026-03-06 18:42:50 +0400

    salam

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -107,4 +107,4 @@ class DeepNeuralNetwork:
         A = self.forward_prop(X)[0]
         prediction = np.where(A >= 0.5, 1, 0)
         cost = self.cost(Y, A)
-        return cost, prediction
+        return prediction, cost

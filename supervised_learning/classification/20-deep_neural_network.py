ed008463c2dfe29ce29a495ba315d73a767b8e6f
Semedw <smusazade2000@gmail.com>
2026-03-06 18:40:22 +0400

    salam

---
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+'''
+deep neural network
+'''
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''
+    building a deep neural network
+    '''
+
+    def __init__(self, nx, layers):
+        '''
+        nx - the number of input features -> int
+        layers - the number of nodes in eahc layer of the network -> List
+        e.g. layers[0] represents the the number of nodes in the first layer
+        '''
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.__L = len(layers)  # the number of layers in the neural network
+        self.__cache = {}  # to hold all intermediary values of the network
+        self.__weights = {}
+        for lay in range(self.L):
+            if layers[lay] < 1 or type(layers[lay]) is not int:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
+            if lay == 0:
+                He_val = np.random.randn(layers[lay], nx) * np.sqrt(2 / nx)
+                self.weights["W" + str(lay + 1)] = He_val
+            if lay > 0:
+                He_val1 = np.random.randn(layers[lay], layers[lay - 1])
+                He_val2 = np.sqrt(2 / layers[lay - 1])
+                He_val3 = He_val1 * He_val2
+                self.weights["W" + str(lay + 1)] = He_val3
+
+    @property
+    def L(self):
+        '''
+        L getter
+        '''
+        return self.__L
+
+    @property
+    def cache(self):
+        '''
+        cache getter
+        '''
+        return self.__cache
+
+    @property
+    def weights(self):
+        '''
+        weights getter
+        '''
+        return self.__weights
+
+    @staticmethod
+    def sigmoid(x):
+        '''
+        sigmoid function
+        '''
+        return 1 / (1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        '''
+        forward propagation
+        '''
+        self.__cache['A0'] = X
+
+        for lay in range(self.__L):
+            W = self.__weights[f'W{lay+1}']
+            b = self.__weights[f'b{lay+1}']
+            Z = np.matmul(W, self.__cache[f'A{lay}']) + b
+            A = self.sigmoid(Z)
+            self.__cache[f'A{lay+1}'] = A
+        return self.__cache[f'A{self.__L}'], self.__cache
+
+    def cost(self, Y, A):
+        '''
+        calculates the cost of model using logistic regression
+        Y - correct labels
+        A - activated outputs
+        '''
+
+        m = Y.shape[1]
+        loss = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001-A))
+        cost = (1 / m) * np.sum(loss)
+        return cost
+
+    def evaluate(self, X, Y):
+        '''
+        evaluates the neural network's predictions
+        X - input data
+        Y - correct labels
+        '''
+
+        A = self.forward_prop(X)
+        prediction = np.where(A>=0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return cost, prediction

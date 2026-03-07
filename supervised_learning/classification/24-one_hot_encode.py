0139dcf3dbc4e880c2087aed158b4646b6183ee9
Semedw <smusazade2000@gmail.com>
2026-03-07 13:12:59 +0400

    try

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+one-hot encode
+'''
+
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    '''
+    converts a numeric label vector to one hot matrix
+    Y - ndarray with shape (m, ) containing numeric label class
+    classes - the maximum number of classes found in Y
+    '''
+    samples = Y.size
+    one_hot_matrix = np.zeros((samples, classes), dtype=float)
+    one_hot_matrix[np.arange(samples), Y] = 1.0

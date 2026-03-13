2779a4de64076fbb1183993554c982d7bb18bf52
Semedw <smusazade2000@gmail.com>
2026-03-13 22:45:17 +0400

    salam

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -16,6 +16,7 @@ def save_weights(network, filename, save_format='keras'):
 
     network.save_weights(filename, save_format)
 
+
 def load_weights(network, filename):
     '''
     network - the model whose weights should be saved

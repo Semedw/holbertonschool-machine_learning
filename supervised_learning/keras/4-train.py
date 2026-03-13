1c843bf9f8a2b7c8cc78bae751f9d6641a600fc6
Semedw <smusazade2000@gmail.com>
2026-03-13 22:03:43 +0400

    salam

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -25,12 +25,12 @@ def train_model(network, data, labels, batch_size,
     '''
 
     history = network.fit(
-        x=data,
-        y=labels,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle
+            x=data,
+            y=labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle
     )
 
     return history

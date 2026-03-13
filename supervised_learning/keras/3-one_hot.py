e6f0654999795823a663540296d3c9506b2ba770
Semedw <smusazade2000@gmail.com>
2026-03-13 17:33:45 +0400

    salam

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+one hot encoding the label vector
+'''
+
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    '''
+    one-hot encoding matrix
+    '''
+    mat = K.ops.one_hot(labels, classes)
+    return mat

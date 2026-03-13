c8434105ff5a204028eb15ca2929de919886ed7a
Semedw <smusazade2000@gmail.com>
2026-03-13 22:21:47 +0400

    salam

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -24,6 +24,12 @@ def train_model(network, data, labels, batch_size,
     returns: History object generated after training the model
     '''
 
+    network.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
     history = network.fit(
             x=data,
             y=labels,

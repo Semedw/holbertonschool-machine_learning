08e4efba8b54698540e4f418818af8b7cf2c903b
Semedw <smusazade2000@gmail.com>
2026-03-13 16:44:06 +0400

    salam

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -17,25 +17,25 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     keep_prob - the probability that a node will be kept for dropout
     '''
 
-    model = K.models.Sequential() # this means: layer 1 > layer 2 > layer3 > output
+    model = K.models.Sequential()  # this means: layer 1 > layer 2 > output
 
     for i in range(len(layers)):
-        if i == 0: # specifying input shape only in the first layer
-            model.add(K.layers.Dense(
-            layers[i],
-            activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha),
-            input_shape=(nx,)
-        ))
-        else:
+        if i == 0:  # specifying input shape only in the first layer
             model.add(K.layers.Dense(
                 layers[i],
                 activation=activations[i],
-                kernel_regularizer=K.regularizers.l2(lambtha)
+                kernel_regularizer=K.regularizers.l2(lambtha),
+                input_shape=(nx,)
+        ))
+        else:
+            model.add(K.layers.Dense(
+                    layers[i],
+                    activation=activations[i],
+                    kernel_regularizer=K.regularizers.l2(lambtha)
             ))
-        
+
         # Add dropout except the last layer
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
-        
+
     return model

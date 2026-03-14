a28eafc14ef697df04d4a2736c079aa7ed01ebf8
Semedw <smusazade2000@gmail.com>
2026-03-14 23:45:53 +0400

    salam

---
+++ supervised_learning/optimization/0-norm_constant.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+normalizing constants
+'''
+
+import numpy as np
+import tensorflow as tf
+
+
+def normalization_constants(X):
+    '''
+    X - ndarray of shape (m, nx):
+        nx - the number of features
+        m - the number of data points
+    '''
+    mean = np.mean(X, axis=0)
+    stddev = np.std(X, axis=0)
+    return mean, stddev

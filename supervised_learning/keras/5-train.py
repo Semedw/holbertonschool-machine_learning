d1bd2fb00ce649af20a1059005956da8e885702b
Semedw <smusazade2000@gmail.com>
2026-03-14 11:42:26 +0400

    salam

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -33,5 +33,5 @@ def train_model(network, data, labels, batch_size,
             validation_data=validation_data,
             shuffle=shuffle
     )
-    print(history.history['accuracy'])
+    print(history.history['acc'])
     return history

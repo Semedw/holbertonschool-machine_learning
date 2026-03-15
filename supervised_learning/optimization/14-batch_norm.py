c372131ff689138038ff28bf2c426f4f243c1ef4
Semedw <smusazade2000@gmail.com>
2026-03-15 22:30:45 +0400

    salam

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,52 +1,24 @@
 #!/usr/bin/env python3
-'''
-batch normalization upgraded
-'''
-
+"""A module that does the trick"""
 import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    '''
-    creates a batch normalization layer for a neural network in tensorflow
-
-    Parameters:
-    prev: output from the previous layer
-    n: number of nodes in the layer to be created
-    activation: activation function that should be used on the output of the
-    layer
-
-    Returns:
-    The activated output of the batch normalization layer
-    '''
-    initializer = tf.keras.initializers.VarianceScaling(
-        scale=2.0, 
-        mode='fan_avg'
+    """Creates a batch normalization layer"""
+    active = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(n, activation=None,
+                                  kernel_initializer=active,
+                                  name='layer')(prev)
+    mu, sigma_2 = tf.nn.moments(layer, axes=[0])
+    gamma = tf.Variable(initial_value=tf.constant(1.0, shape=[n]),
+                        name='gamma')
+    beta = tf.Variable(initial_value=tf.constant(0.0, shape=[n]),
+                       name='beta')
+    Z_b_norm = tf.nn.batch_normalization(
+        layer, mu,
+        sigma_2,
+        offset=beta,
+        scale=gamma,
+        variance_epsilon=1e-7
     )
-
-    # Base Dense layer
-    # Note: use_bias=True is default, but BN will effectively 
-    # override the Dense bias via its own 'beta' parameter.
-    dense_layer = tf.keras.layers.Dense(
-        units=n,
-        kernel_initializer=initializer,
-        use_bias=True
-    )
-    
-    Z = dense_layer(prev)
-
-    # Batch Normalization layer
-    # gamma_initializer='ones' and beta_initializer='zeros' are defaults
-    batch_norm_layer = tf.keras.layers.BatchNormalization(
-        epsilon=1e-7,
-        gamma_initializer='ones',
-        beta_initializer='zeros'
-    )
-    
-    Z_norm = batch_norm_layer(Z)
-
-    # Apply activation function if provided
-    if activation is not None:
-        return activation(Z_norm)
-
-    return Z_norm
+    return activation(Z_b_norm)

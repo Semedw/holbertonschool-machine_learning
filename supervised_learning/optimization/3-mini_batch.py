7e7c8ab5e2ad8e8384452ca2ffba90febfb73ab7
Semedw <smusazade2000@gmail.com>
2026-03-15 21:49:59 +0400

    salam

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -6,6 +6,8 @@ mini batch gradient descent
 
 import numpy as np
 
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
 
 def create_mini_batches(X, Y, batch_size):
     '''
@@ -22,11 +24,11 @@ def create_mini_batches(X, Y, batch_size):
     Returns: a list of tuples, each tuple is (X_batch, Y_batch)
     '''
 
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
     m = X.shape[0]
     mini_batches = []
     for i in range(0, m, batch_size):
-        X_batch = X[i:i + batch_size]
-        Y_batch = Y[i:i + batch_size]
+        X_batch = X_shuffled[i:i + batch_size]
+        Y_batch = Y_shuffled[i:i + batch_size]
         mini_batches.append((X_batch, Y_batch))
-
     return mini_batches

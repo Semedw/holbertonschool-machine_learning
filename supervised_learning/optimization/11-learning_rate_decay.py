aab6cc8ba06a96d1058b0b9719513d6fff4af955
Semedw <smusazade2000@gmail.com>
2026-03-15 22:17:03 +0400

    pycodestyle

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -14,7 +14,8 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     alpha: original learning rate
     decay_rate: weight used to determine the rate at which alpha will decay
     global_step: number of passes of gradient descent that have elapsed
-    decay_step: number of passes of gradient descent that should occur before alpha is decayed
+    decay_step: number of passes of gradient descent that should occur
+    before alpha is decayed
 
     Returns:
     Updated learning rate

efacc77101708a14968644c689e336ad99229296
Semedw <smusazade2000@gmail.com>
2026-03-19 18:17:38 +0400

    salam

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -17,6 +17,10 @@ def l2_reg_create_layer(prev, n, activation, lambd):
     layer = tf.keras.layers.Dense(
         n,
         activation=activation,
+        kernel_initializer = tf.keras.initializers.VarianceScaling(
+            scale=2.0,
+            mode='fan_avg'
+        ),
         kernel_regularizer=tf.keras.regularizers.L2(lambd)
     )
     return layer(prev)

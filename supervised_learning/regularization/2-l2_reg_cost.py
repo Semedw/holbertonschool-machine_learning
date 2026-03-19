ead155363a5b1485884b98d9b5a4c3295a2a09dd
Semedw <smusazade2000@gmail.com>
2026-03-19 17:45:46 +0400

    regularization cost

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -14,12 +14,8 @@ def l2_reg_cost(cost, model):
     Returns: a tensor containing the total cost for each
             layer of the network, accounting for L2 regularization
     '''
-    l2_costs = []
-    for i in range(len(model.layers)):
-        if hasattr(model.layers[i], 'kernel_regularizer'):
-            l2_cost = model.layers[i].kernel_regularizer(model.layers[i].kernel)
-            l2_costs.append(l2_cost)
-
-    total_l2_cost = tf.add_n(l2_costs)
-
-    return cost + total_l2_cost
+    reg_cost = cost
+    for layer in model.layers:
+        for loss in layer.losses:
+            reg_cost += loss
+    return reg_cost

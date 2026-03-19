abe78ec7e922fe297aff4b547982b55d05f5a83d
Semedw <smusazade2000@gmail.com>
2026-03-19 17:11:07 +0400

    pycodestyle

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,10 +8,11 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambd, L):
     '''
-    Y - a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
+    Y - a one-hot numpy.ndarray of shape (classes, m) that contains
+    the correct labels for the data
         - classes: the number of classes
         - m: the number of data points
-    
+
     weights - a dictionary of the weights and biases of the neural network
     cache - a dictionary of the outputs of each layer of the neural network
     alpha - the learning rate

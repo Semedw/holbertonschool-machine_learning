bdb5d2564dfb2ad0a7010c04817a8e0cfd8a6f5d
Semedw <smusazade2000@gmail.com>
2026-03-23 14:31:22 +0400

    same convolution

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+same convolution
+'''
+
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    '''
+    images - an ndarray with shape (m, h, w) containing
+            multiple grayscale images
+
+            m - the number of images
+            h - the height in pixels of the image
+            w - the width in pixels of the images
+
+    kernel - an ndarray with shape (kh, kw) containing
+            the kernel for the convolution
+
+            kh - the height of the kernel
+            kw - the width of the kernel
+
+    Returns: a numpy.ndarray containing the convolved images
+    '''
+
+    images = np.pad(images)
+
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    h_out = h - kh + 1
+    w_out = w - kw + 1
+
+    convolved = np.zeros((m, h_out, w_out))
+
+    for i in range(h_out):
+        for j in range(w_out):
+            image = images[:, i: i + kh, j: j + kw]
+            convolved[:, i, j] = np.sum(image*kernel, axis=(1, 2))
+    return convolved

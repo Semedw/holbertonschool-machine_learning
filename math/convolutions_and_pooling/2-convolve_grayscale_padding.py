2748d932a54242f44c033cca708ed241c1bc20c6
Semedw <smusazade2000@gmail.com>
2026-03-24 20:21:32 +0400

    trying smth

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -29,13 +29,16 @@ def convolve_grayscale_padding(images, kernel, padding):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
+    h_out = h - kh + 1
+    w_out = w - kw + 1
+
     images_padded = np.pad(images, padding,
                            mode='constant', constant_values=0)
 
-    convolved = np.zeros((m, h, w)) 
+    convolved = np.zeros((m, h, w))
 
-    for i in range(h):
-        for j in range(w):
+    for i in range(h_out):
+        for j in range(w_out):
             image = images_padded[:, i: i + kh, j: j + kw] 
             convolved[:, i, j] = np.sum(image*kernel, axis=(1, 2)) 
     return convolved

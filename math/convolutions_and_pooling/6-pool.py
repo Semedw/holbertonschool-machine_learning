a31a07ce5ada32852700d35e4ad1af0c2c8ebea6
Semedw <smusazade2000@gmail.com>
2026-03-26 14:52:34 +0400

    added the number of channels to the shape of output

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -33,11 +33,11 @@ def pool(images, kernel_shape, stride, mode='max'):
     h_out = (h - kh) // sh + 1
     w_out = (w - kw) // sw + 1
 
-    output = np.zeros((m, h_out, w_out))
+    output = np.zeros((m, h_out, w_out, c))
 
     for i in range(h_out):
         for j in range(w_out):
-            patch = images[:, i * sh: i * sh + kh, j * sw: j * sw + kw]
+            patch = images[:, i * sh: i * sh + kh, j * sw: j * sw + kw, :]
             if mode == 'max':
                 output[:, i, j] = np.max(patch, axis=(1, 2))
             elif mode == 'avg':

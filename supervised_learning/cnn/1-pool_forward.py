ef8857a5041ee187f03f3b00a3e963df39a0c5af
Semedw <smusazade2000@gmail.com>
2026-04-02 18:30:43 +0400

    fixed axis

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -41,10 +41,10 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode="max"):
         for h in range(nh):
             for w in range(nw):
                 for c in range(c_prev):
-                    patch = a_prev[:, h * sh: h * sh + kh, w * sw: w * sw + kw]
+                    patch = a_prev[h * sh: h * sh + kh, w * sw: w * sw + kw]
                     if mode == 'max':
-                        output[i, h, w, c] = np.max(patch, axis=(1, 2, 3, 4))
-                    elif mode == 'mean':
-                        output[i, h, w, c] = np.mean(patch, axis=(1, 2, 3, 4))
+                        output[i, h, w, c] = np.max(patch)
+                    elif mode == 'avg':
+                        output[i, h, w, c] = np.mean(patch)
 
     return output

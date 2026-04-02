f9a38eba56ba81c6ba25a8f653770d0bb68af5e0
Semedw <smusazade2000@gmail.com>
2026-04-02 17:56:10 +0400

    pycodestyle

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -78,7 +78,9 @@ Z[i, h, w, c] = np.sum(a_slice * W[:, :, :, c]) + b[0, 0, 0, c]
                     a_slice = a_prev_pad[vert_start:vert_end,
                                          horiz_start:hortiz_end]
 
-                    Z[i, h, w, c] = np.sum(a_slice * W[:, :, :, c]) + \ 
-                                    b[0, 0, 0, c]
+                    Z[i, h, w, c] = np.sum(a_slice * W[:, :, :, c]) +  b[0,
+                                                                         0,
+                                                                         0,
+                                                                         c]
 
     return activation(Z)

945a99ba85fb80bfb93aa714d778135652541153
Semedw <smusazade2000@gmail.com>
2026-04-04 10:16:32 +0400

    pycodestyle

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -26,16 +26,16 @@ def lenet5(X):
                             activation='relu',
                             kernel_initializer=initializer)(X)
     pool1 = K.layers.MaxPooling2D(pool_size=2,
-                                      strides=2)(conv1)
+                                  strides=2)(conv1)
     conv2 = K.layers.Conv2D(filters=16,
                             kernel_size=5,
                             padding='valid',
                             activation='relu',
                             kernel_initializer=initializer)(pool1)
     pool2 = K.layers.MaxPooling2D(pool_size=2,
-                                      strides=2)(conv2)
+                                  strides=2)(conv2)
     flatten = K.layers.Flatten()(pool2)
-    fc1 = K.layers.Dense(units=120, 
+    fc1 = K.layers.Dense(units=120,
                          activation='relu',
                          kernel_initializer=initializer)(flatten)
     fc2 = K.layers.Dense(units=84,

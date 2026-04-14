f6048bb126a36f49a9cbdb8f98afe96e10c7f402
Semedw <smusazade2000@gmail.com>
2026-04-14 18:32:02 +0400

    pycodestyle

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -23,17 +23,39 @@ def inception_block(A_prev, filters):
 
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    branch1 = K.layers.Conv2D(F1, (1, 1), padding='same', activation='relu')(A_prev)
+    branch1 = K.layers.Conv2D(F1,
+                              (1, 1),
+                              padding='same',
+                              activation='relu')(A_prev)
 
-    branch2 = K.layers.Conv2D(F3R, (1, 1), padding='same', activation='relu')(A_prev)
-    branch2 = K.layers.Conv2D(F3, (3, 3), padding='same', activation='relu')(branch2)
+    branch2 = K.layers.Conv2D(F3R,
+                              (1, 1),
+                              padding='same',
+                              activation='relu')(A_prev)
+    branch2 = K.layers.Conv2D(F3,
+                              (3, 3),
+                              padding='same',
+                              activation='relu')(branch2)
 
-    branch3 = K.layers.Conv2D(F5R, (1, 1), padding='same', activation='relu')(A_prev)
-    branch3 = K.layers.Conv2D(F5, (5, 5), padding='same', activation='relu')(branch3)
+    branch3 = K.layers.Conv2D(F5R,
+                              (1, 1),
+                              padding='same',
+                              activation='relu')(A_prev)
+    branch3 = K.layers.Conv2D(F5,
+                              (5, 5),
+                              padding='same',
+                              activation='relu')(branch3)
 
-    branch4 = K.layers.MaxPooling2D((3, 3), strides=(1, 1), padding='same')(A_prev)
-    branch4 = K.layers.Conv2D(FPP, (1, 1), padding='same', activation='relu')(branch4)
+    branch4 = K.layers.MaxPooling2D((3, 3),
+                                    strides=(1, 1),
+                                    padding='same')(A_prev)
+    branch4 = K.layers.Conv2D(FPP,
+                              (1, 1),
+                              padding='same',
+                              activation='relu')(branch4)
 
-    output = K.layers.Concatenate(axis=-1)([branch1, branch2, branch3, branch4])
+    output = K.layers.Concatenate(axis=-1)(
+        [branch1, branch2, branch3, branch4]
+        )
 
     return output

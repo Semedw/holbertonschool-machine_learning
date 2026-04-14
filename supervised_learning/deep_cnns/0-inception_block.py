3db1e3ac8340e4da27f5bb5807bfa861d420828f
Semedw <smusazade2000@gmail.com>
2026-04-14 18:33:19 +0400

    pycode

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -9,15 +9,19 @@ from tensorflow import keras as K
 def inception_block(A_prev, filters):
     '''
     A_prev - the output from the previous layer
-    filters - a tuple or list containing F1, F3R, F3,F5R, F5, FPP, respectively:
+    filters - a tuple or list containing F1, F3R, F3,F5R, F5, FPP:
         F1 - the number of filters in the 1x1 convolution
-        F3R - the number of filters in the 1x1 convolution before the 3x3 convolution
+        F3R - the number of filters in the 1x1 convolution
+                before the 3x3 convolution
         F3 - the number of filters in the 3x3 convolution
-        F5R - the number of filters in the 1x1 convolution before the 5x5 convolution
+        F5R - the number of filters in the 1x1 convolution
+                before the 5x5 convolution
         F5 - the number of filters in the 5x5 convolution
-        FPP - the number of filters in the 1x1 convolution after the max pooling
+        FPP - the number of filters in the 1x1 convolution
+                after the max pooling
 
-    All convolutions inside the inception block should use a rectified linear activation (ReLU)
+    All convolutions inside the inception block should use a rectified
+    linear activation (ReLU)
     Returns: the concatenated output of the inception block
     '''
 

9afedad0d6231ad2acd1cc13e9257e3dcf04897d
Semedw <smusazade2000@gmail.com>
2026-04-16 18:57:18 +0400

    renat50

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -3,7 +3,7 @@
 renat 50
 '''
 
-import tensorflow.keras as K
+from tensorflow import keras as K
 
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block

ff2dfdb24ae394751c45810d8340694d9249915b
Semedw <smusazade2000@gmail.com>
2026-04-16 18:53:43 +0400

    resnet50

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -5,9 +5,8 @@ renat 50
 
 from tensorflow import keras as K
 
-
 identity_block = __import__("2-identity_block").identity_block
-projection_block = __import("3-projection_block").projection_block
+projection_block = __import__("3-projection_block").projection_block
 
 def resnet():
     '''

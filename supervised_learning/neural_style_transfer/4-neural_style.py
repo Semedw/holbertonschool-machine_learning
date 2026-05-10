5f723ab1f161432e9b6d56299dc3314843fa61cf
Semedw <smusazade2000@gmail.com>
2026-05-10 11:50:15 +0400

    correct shape retrivial

--- supervised_learning/neural_style_transfer/4-neural_style.py
+++ supervised_learning/neural_style_transfer/4-neural_style.py
@@ -153,12 +153,12 @@ class NST:
 
     def layer_style_cost(self, style_output, gram_target):
         """Calculates the style cost for a single layer"""
-        _, c, _ = gram_target.shape
         if not isinstance(style_output, (tf.Tensor, tf.Variable)) or \
                 len(style_output.shape) != 4:
             raise TypeError("style_output must be a tensor of rank 4")
+        c = int(style_output.shape[-1])
         if not isinstance(gram_target, (tf.Tensor, tf.Variable)) or \
-                len(gram_target.shape) != 3:
+                gram_target.shape != (1, c, c):
             raise TypeError(f"gram_target must be a tensor of shape [1, {c}, {c}]")
 
         gram_style = self.gram_matrix(style_output)

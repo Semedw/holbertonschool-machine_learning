0de0f59f6634d877e9e0010cecabc876bfd0799e
Semedw <smusazade2000@gmail.com>
2026-05-17 21:37:38 +0400

    p_init

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -3,11 +3,13 @@
 import numpy as np
 
 
-def pca(X, ndim):
+def P_init(X, perplexity):
     """A function that does the trick"""
-    X_mean = X - np.mean(X, axis=0)
-    U, S, V = np.linalg.svd(X_mean)
-    W = V.T
-    Wr = W[:, :ndim]
-    T = np.dot(X_mean, Wr)
-    return T
+    n, d = X.shape
+    sum_X = np.sum(np.square(X), axis=1)
+    D = (np.add(np.add(-2 * np.matmul(X, X.T), sum_X).T, sum_X))
+    np.fill_diagonal(D, 0)
+    P = np.zeros((n, n))
+    betas = np.ones((n, 1))
+    H = np.log2(perplexity)
+    return D, P, betas, H

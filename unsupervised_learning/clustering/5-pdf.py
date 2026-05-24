d633ef4369d6d3b1b264e2394b3371d3a82a5e33
Semedw <smusazade2000@gmail.com>
2026-05-24 11:21:46 +0400

    pdf of a gaussian distribution

---
+++ unsupervised_learning/clustering/5-pdf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''
+PDF of a Gaussian distribution.
+'''
+
+import numpy as np
+
+
+def pdf(X, m, S):
+    '''
+    Calculates the PDF of a Gaussian distribution.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+            - n is the number of data points
+            - d is the number of dimensions for each data point
+        m: numpy.ndarray of shape (d,) containing the mean of the distribution
+        S: numpy.ndarray of shape (d, d) containing the covariance of the
+        distribution
+    Returns:
+        P: numpy.ndarray of shape (n,) containing the PDF values for each data
+        point, or None on failure
+    '''
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0] or S.shape[0] != S.shape[1]:
+        return None
+    n, d = X.shape
+    S_inv = np.linalg.inv(S)
+    det_S = np.linalg.det(S)
+    if det_S <= 0:
+        return None
+    norm_const = 1.0 / (np.power(2 * np.pi, d / 2) * np.sqrt(det_S))
+    diff = X - m
+    exponent = -0.5 * np.sum(diff @ S_inv * diff, axis=1)
+    P = norm_const * np.exp(exponent)
+    return P

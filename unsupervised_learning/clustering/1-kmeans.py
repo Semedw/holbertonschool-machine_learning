1e5b8f578e2fc1868af879d4259a61846a6aec21
Semedw <smusazade2000@gmail.com>
2026-05-24 10:54:03 +0400

    k-means

---
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+K-means clustering.
+'''
+
+import numpy as np
+
+
+def kmeans(X, k, iterations=1000):
+    '''
+    Performs K-means on a dataset.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+            - n is the number of data points
+            - d is the number of dimensions for each data point
+        k: positive integer, the number of clusters
+        iterations: positive integer, the maximum number of iterations
+    Returns:
+        C: numpy.ndarray of shape (k, d) containing the centroid means for
+        each cluster, or None on failure
+        clss: numpy.ndarray of shape (n,) containing the index of the
+        cluster in C that each data point belongs to, or None on failure
+    '''
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
+        return None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+    n, d = X.shape
+    C = np.random.uniform(np.min(X, axis=0), np.max(X, axis=0), size=(k, d))
+    clss = np.zeros(n, dtype=int)
+    for _ in range(iterations):
+        distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+        new_clss = np.argmin(distances, axis=1)
+        if np.array_equal(clss, new_clss):
+            break
+        clss = new_clss
+        for i in range(k):
+            if np.any(clss == i):
+                C[i] = X[clss == i].mean(axis=0)
+    return C, clss

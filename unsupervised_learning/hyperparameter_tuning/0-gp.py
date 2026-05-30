bc0f75db408596001b62e385932392f415f9c972
Semedw <smusazade2000@gmail.com>
2026-05-30 11:31:21 +0400

    fix: pycodestyle

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -7,13 +7,18 @@ import numpy as np
 
 
 class GaussianProcess:
+    '''
+    Gaussian Process class for regression.
+    '''
     def __init__(self, X_init, y_init, l=1, sigma_f=1):
         '''
         Initialize the Gaussian Process with initial data and hyperparameters.
-        
+
         Parameters:
-        X_init (np.ndarray): Initial input data of shape (n_samples, n_features).
-        y_init (np.ndarray): Initial output data of shape (n_samples,).
+        X_init (np.ndarray): Initial input data of shape
+                            (n_samples, n_features).
+        y_init (np.ndarray): Initial output data of shape
+                            (n_samples,).
         l (float): Length scale hyperparameter.
         sigma_f (float): Output variance hyperparameter.
         '''
@@ -22,14 +27,17 @@ class GaussianProcess:
         self.X = X_init
         self.Y = y_init
         self.K = self.kernel(X_init, X_init)
+
     def kernel(self, X1, X2):
         '''
         Compute the kernel matrix between two sets of input data.
-        
+
         Parameters:
-        X1 (np.ndarray): First input data of shape (n_samples, n_features).
-        X2 (np.ndarray): Second input data of shape (n_samples, n_features).
-        
+        X1 (np.ndarray): First input data of shape
+                            (n_samples, n_features).
+        X2 (np.ndarray): Second input data of shape
+                            (n_samples, n_features).
+
         Returns:
         np.ndarray: Kernel matrix of shape (n_samples, n_samples).
         '''

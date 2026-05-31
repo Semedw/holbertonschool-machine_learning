e9e5584093846e94abd86bb4a4ec91ad76df8af9
Semedw <smusazade2000@gmail.com>
2026-05-31 23:21:25 +0400

    init bayes opt

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -14,9 +14,39 @@ class BayesianOptimization:
     '''
 
     def __init__(self, f, X_init, y_init, bounds, ac_samples, l=1.0,
-                 sigma_f=1.0, xsi=0.01, minimizer=True):
+                 sigma_f=1.0, xsi=0.01, minimize=True):
+        """
+        init method for bayesian optimization
+        Args:
+            f: the black-box function to be optimized
+            X_init: numpy.ndarray of shape (t, 1) representing the inputs
+                    already sampled with the black-box function
+            Y_init: numpy.ndarray of shape (t, 1) representing the outputs
+                    of the black-box function for each input in X_init
+                    t: the number of initial samples
+            bounds: tuple of (min, max) representing the bounds of the space
+                    in which to look for the optimal point
+            ac_samples: the number of samples that should be analyzed during
+                        acquisition
+            l: the length parameter for the kernel
+            sigma_f: the standard deviation given to the output of the
+                    black-box function
+            xsi: the exploration-exploitation factor for acquisition
+            minimize: bool determining whether optimization should be performed
+                    for minimization (True) or maximization (False)
+        """
+        # black-box function
         self.f = f
-        self.gp = GP(X_init, y_init, bounds, l, sigma_f, minimizer)
-        self.ac_samples = ac_samples
+
+        # Gaussian Process
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+
+        # X_s all acquisition sample
+        X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
+        self.X_s = X_s.reshape(-1, 1)
+
+        # exploration-explotation
         self.xsi = xsi
-        self.minimizer = minimizer
+
+        # minimization versus maximization
+        self.minimize = minimize
